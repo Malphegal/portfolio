4850cd51ef26858b59ff12a8a1b2982270fcb30f
Malphegal <nateag_96@hotmail.fr>
2020-02-16 20:31:46 +0100

    Ajout des fichiers de base sur Github.

---
+++ index.php
@@ -0,0 +1,32 @@
+<!-- https://startbootstrap.com/previews/resume/ -->
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<title> Portfolio de Malphegal - Gaëtan</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+    <body>
+		<nav>
+			<div></div>
+		</nav>
+        <main>
+			<section id="baseinformation">
+				<div>
+					<a>Gaëtan&nbsp;<span>Martini</span></a>
+					<img src="img/logo/contact.png" alt="Logo de contact">
+					<a>anacin@hotmail.fr</a>
+				</div>
+			</section>
+			<section>
+				<div>a</div>
+			</section>
+			<section>
+				<div>b</div>
+			</section>
+        </main>
+        <footer>
+			<div></div>
+        </footer>
+    </body>
+</html>
